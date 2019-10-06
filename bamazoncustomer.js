0719a7998504e2b4912577ae4dcc7bb74c4ec44c
Eric LaMacchia <ericlamacchia1@icloud.com>
2019-10-06 10:56:55 -0400

    Completed the bamazon customer, added my logic, styled the table a little bit, mayd decide to add more items later, however im not sure yet.

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -1,46 +1,111 @@
+//These are the npm packages that are required
 var mysql = require("mysql");
-var inquirer = require("inquirer")
-
+var inquirer = require("inquirer");
+var Table = require("cli-table2");
+//This is where i set up my connection
 var connection = mysql.createConnection({
   host: "localhost",
-
-  
-  // Your port; if not 3306
-  port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "A13bbcda!",
-  database: "bamazon_DB"
+  database: "bamazon_DB",
+  port: 3306
 });
-// Creat the connection with the server
-
-connection.connect(function(err) {
+// This is where the actual connection is being made
+connection.connect();
+//This is where I will be displaying the products that i have for sale
+var display = function() {
+  connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
-    // Run the start function after the connection is made to prompt the user
-    start();
-  });
-  
-  function start() {
-    inquirer
-     .prompt({
-       name: "welcome",
-       type: "list",
-       message: "Welcome to bamazon, would you like to search items",
-       choices: ["View Items", "Buy Items", "EXIT"]
-     })
-     .then(function(answer) {
-       // Based on the andswer either call the bid or post the functions
-       if (answer.postOrBid === "View Items") {
-         viewItems();
-       }
-       else if(answer.postOrBid === "Buy Items") {
-         buyItems();
-       } else{
-         connection.end();
-       }
-     });
-  }
-  
+    console.log("-----------------------------");
+    console.log("      Welcome To the Bamazon Homie!    ");
+    console.log("-----------------------------");
+    console.log("");
+    console.log("Here are our products!");
+    console.log("");
+    var table = new Table({
+      head: ["Product Id", "Product Description", "Cost"],
+      colWidths: [12, 50, 8],
+      colAligns: ["center", "left", "right"],
+      style: {
+        head: ["aqua"],
+        compact: true
+        // 'padding-right' : 1,
+      }
+    });
+// This is where i need to actually push the specific data to the specific parts of the table
+    for (var i = 0; i < res.length; i++) {
+      table.push([res[i].id, res[i].products_id, res[i].price]);
+    }
+
+    console.log(table.toString());
+    console.log("");
+    shopping();
+  }); //End Connection to products
+};
+//Here is where i need to make it possible to input a products id number so that you may actually purchase it.
+var shopping = function() {
+  inquirer
+    .prompt({
+      name: "productToBuy",
+      type: "input",
+      message: "Please enter the Product Id of the item you wish to purchase.!"
+    })
+    .then(function(answer1) {
+      var selection = answer1.productToBuy;
+      connection.query("SELECT * FROM products WHERE Id=?", selection, function(
+        err,
+        res
+      ) 
+      {
+        // I need to make an error in case the product doesnt exist or if the item is sold out.
+        if (err) throw err;
+        if (res.length === 0) {
+          console.log(
+            "Currently we do not have this product, Sorry!"
+          );
+        //The option for the amount that you wish to purchase needs to go here
+          shopping();
+        } else {
+          inquirer
+            .prompt({
+              name: "quantity",
+              type: "input",
+              message: "How many items would you like to purchase?"
+            })
+            .then(function(answer2) {
+              var quantity = answer2.quantity;
+              if (quantity > res[0].stock_quantity) {
+                console.log(
+                  "Our Apologies we only have " +
+                    res[0].stock_quantity +
+                    " items of the product selected"
+                );
+                shopping();
+              } else {
+                console.log("");
+                console.log(res[0].products_id + " purchased");
+                console.log(quantity + " qty @ $" + res[0].price);
+
+                var newQuantity = res[0].stock_quantity - quantity;
+                connection.query(
+                  "UPDATE products SET stock_quantity = " +
+                    newQuantity +
+                    " WHERE id = " +
+                    res[0].id,
+                  function(err, resUpdate) {
+                    if (err) throw err;
+                    console.log("");
+                    console.log("Your Order has been Processed");
+                    console.log("Thank you for Shopping with us...!");
+                    console.log("");
+                    connection.end();
+                  }
+                );
+              }
+            });
+        }
+      });
+    });
+};
+
+display();
--- schema.sql
+++ schema.sql
@@ -4,13 +4,13 @@ CREATE DATABASE bamazon_DB;
 USE bamazon_DB;
 
 CREATE TABLE products (
-id INT AUTO_INCREMENT NOT NULL,
+item_id INT AUTO_INCREMENT NOT NULL,
 product_name VARCHAR (255) NOT NULL,
 department_name VARCHAR (255) NOT NULL,
 price DECIMAL(10,2) NOT NULL,
 stock_quanity INT (10) NOT NULL,
 primary key (id)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 
 INSERT INTO products (product_name, department_name, price, stock_quanity)
 VALUES ("Corvette", "Car", 50000.00, 10);
